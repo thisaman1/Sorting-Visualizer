72b1ec3b33825096e9616f1ddc6ce9fae451b3b0
Aman <71255038+thisaman1@users.noreply.github.com>
2024-12-29 21:26:27 +0530

    Update bubble_sort.js

--- scripts/bubble_sort.js
+++ scripts/bubble_sort.js
@@ -13,6 +13,7 @@ function Bubble()
 
     for(var i=0;i<array_size-1;i++)
     {
+        var swapped = false;
         for(var j=0;j<array_size-i-1;j++)
         {
             div_update(divs[j],div_sizes[j],"yellow");//Color update
@@ -28,10 +29,13 @@ function Bubble()
 
                 div_update(divs[j],div_sizes[j], "red");//Height update
                 div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+
+                swapped = true;
             }
             div_update(divs[j],div_sizes[j], "blue");//Color updat
         }
         div_update(divs[j],div_sizes[j], "green");//Color update
+        if (!swapped) break;
     }
     div_update(divs[0],div_sizes[0], "green");//Color update
 
